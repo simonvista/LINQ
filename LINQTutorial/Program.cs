5a1e6068b2783d423bc74149e1df74adcfa04995
YL <simon-ling@hotmail.com>
2021-08-16 18:38:48 -0400

    0245 is done

--- LINQTutorial/Program.cs
+++ LINQTutorial/Program.cs
@@ -10,12 +10,28 @@ namespace LINQTutorial
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //1. data source
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //2. create query
+            var divBy2Query = from num in nums
+                              where num % 2 == 0
+                              select num;
+            var greaterThan5Query = from num in nums
+                                    where num > 5
+                                    select num;
+            //3. execute query
+            foreach (var item in divBy2Query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------");
+            foreach (var item in greaterThan5Query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();      
         }
+
+
     }
 }
